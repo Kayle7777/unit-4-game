128f78d46c84b9a62111f07ef4bb11b4b42c08eb
Kayle7777 <jessewebb2@gmail.com>
2018-06-24 12:37:06 -0500

    Stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,8 +9,10 @@
 //   btn.randNumber = Math.floor(Math.random() * 12) + 1;
 // }
 
-// I had a fake constructor function() that would run when new Button was done,
-// But a friend told me the proper syntax for a REAL constructor.
+// I hadn't even thought of using Python-like Classes until I read about them on mozilla docs, because I assumed JS couldn't do it.
+// This looks very familiar to some of my old Python3 stuff.
+
+// Quoted from caniuse.com -- "ES6 classes are syntactical sugar to provide a much simpler and clearer syntax to create objects and deal with inheritance."
 
 
 class Button {
