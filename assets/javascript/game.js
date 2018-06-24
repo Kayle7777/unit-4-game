b60ccba5eb8ceca02aa695a9c299b6d462ebb9af
Kayle7777 <jessewebb2@gmail.com>
2018-06-24 18:46:53 -0500

    Was overcomplicating it.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,8 @@
-// var buttons = $.map($("#buttons").children(), (x) => {
-//   new Button(x);
-//   $(x).on('click', () => {
-//     console.log(x.randNumber);
-//   })
-// })
-//
-// function Button(btn) {
-//   btn.randNumber = Math.floor(Math.random() * 12) + 1;
-// }
-
-// Friend told me to search for JS classes / similar to Python ones
-
-// Quoted from caniuse.com -- "ES6 classes are syntactical sugar to provide a much simpler and clearer syntax to create objects and deal with inheritance."
-
-
-class Button {
-  constructor () {
-      this.randNumber = Math.floor(Math.random() * 12) + 1;
-  }
-}
+// Was overcomplicated it.
 
 let buttons = $.map($("#buttons").children(), (element) => {
-  let eachButton = new Button(element);
-  console.log(eachButton);
+  element.randNumber = this.randNumber = Math.floor(Math.random() * 12) + 1;
   $(element).on('click', () => {
-    console.log(eachButton.randNumber);
+    console.log(element.randNumber);
   })
 })
