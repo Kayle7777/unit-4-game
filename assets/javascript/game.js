0672df07d6aef052835d73c1a1cb8017b215b7e1
Kayle7777 <jessewebb2@gmail.com>
2018-06-24 12:24:06 -0500

    more clear comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,14 @@
+// var buttons = $.map($("#buttons").children(), (x) => {
+//   new Button(x);
+//   $(x).on('click', () => {
+//     console.log(x.randNumber);
+//   })
+// })
+//
+// function Button(btn) {
+//   btn.randNumber = Math.floor(Math.random() * 12) + 1;
+// }
+
 // I had a fake constructor function() that would run when new Button was done,
 // But a friend told me the proper syntax for a REAL constructor.
 
