18f922906bde5b953041fddf56a6948d977fd22a
Jesse Webb <jessewebb2@gmail.com>
2018-06-25 07:07:38 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // Was overcomplicated it.
 
 let buttons = $.map($("#buttons").children(), (element) => {
-  element.randNumber = this.randNumber = Math.floor(Math.random() * 12) + 1;
+  element.randNumber = Math.floor(Math.random() * 12) + 1;
   $(element).on('click', () => {
     console.log(element.randNumber);
   })
